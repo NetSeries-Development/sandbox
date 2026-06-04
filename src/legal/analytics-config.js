7a4b40a55fcaae338b4605e29964f216e72ab3bf
Koen <koen@netseries.dev>
2026-06-04 19:56:15 +0200

    Update analytics-config.js

--- src/legal/analytics-config.js
+++ src/legal/analytics-config.js
@@ -1,6 +1,3 @@
-// analytics-config.js
-
-// 1. Cookiebot Script dynamisch laden
 var cookiebotScript = document.createElement("script");
 cookiebotScript.src = "https://consent.cookiebot.com/uc.js";
 cookiebotScript.id = "Cookiebot";
@@ -9,13 +6,11 @@ cookiebotScript.setAttribute("data-blockingmode", "auto");
 cookiebotScript.type = "text/javascript";
 document.head.appendChild(cookiebotScript);
 
-// 2. Google Analytics Base Script laden (wacht op toestemming 'statistics')
 var gaScript = document.createElement("script");
 gaScript.src = "https://www.googletagmanager.com/gtag/js?id=G-K184EY8SB4";
 gaScript.setAttribute("data-cookieconsent", "statistics");
 document.head.appendChild(gaScript);
 
-// 3. Google Analytics initialiseren (wacht op toestemming 'statistics')
 var gaInit = document.createElement("script");
 gaInit.type = "text/plain";
 gaInit.setAttribute("data-cookieconsent", "statistics");
